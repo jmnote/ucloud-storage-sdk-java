fc68187155e91715d510df6bac961ace87044fe0
jmnote <banking@nate.com>
2016-08-02 17:00:06 +0900

    not access properties file
    not access properties file

--- src/main/java/com/rackspacecloud/client/cloudfiles/FilesUtil.java
+++ src/main/java/com/rackspacecloud/client/cloudfiles/FilesUtil.java
@@ -37,12 +37,6 @@ public class FilesUtil
     private static synchronized void loadPropertiesFromClasspath() throws IOException
     {
         props = new Properties();
-        InputStream io = FilesUtil.class.getClassLoader().getResourceAsStream(file);
-        if (io == null)
-        {
-            throw new FileNotFoundException("Property file '" + file + "' not found in the classpath.");
-        }
-        loadProperties(io);
     }
 
 
